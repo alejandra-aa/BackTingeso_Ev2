572cf24c2653c99546fdb19c4103f30a0765c929
Alejandra <alejandra.aguilera.a@usach.cl>
2022-01-16 19:09:32 -0300

    Junit test listos

--- demo/src/test/java/com/example/demo/junitTest.java
+++ demo/src/test/java/com/example/demo/junitTest.java
@@ -3,17 +3,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.json.JSONObject;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpDelete;
-import static org.hamcrest.Matchers.*;
+import org.apache.http.client.methods.HttpPut;
 import java.io.IOException;
 import org.apache.http.client.ClientProtocolException;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
 import java.net.URI;
 import java.net.URL;
 import java.net.URISyntaxException;
@@ -24,7 +20,6 @@ import org.apache.http.HttpEntity;
 
 @SpringBootTest
 public class junitTest {
-   
    
    @Test
    //Chequea si se puede ingresar un producto
@@ -39,11 +34,7 @@ public class junitTest {
         URL url = new URL("http://localhost:8081/productos/create/" + item.toString());
         URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
         HttpUriRequest request = new HttpPost(uri);
-        //System.out.println(request);
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        HttpEntity httpEntity = httpResponse.getEntity();
-        System.out.println("pasa por el test crearproducto");
-        System.out.println(EntityUtils.toString(httpEntity));
         }catch (IOException e) {
             e.printStackTrace();
         }catch (URISyntaxException e) {
@@ -52,14 +43,26 @@ public class junitTest {
    
 	}
 
-   /*@Test
-    //Revisa que el producto 999 no se encuentra en la api
-    public void getProductoApi() throws ClientProtocolException, IOException {
-        HttpUriRequest request = new HttpGet( "http://localhost:8081/productos/" + 999 );
+   @Test
+    //Realiza un update al producto 2
+    public void updateProducto2() throws URISyntaxException, MalformedURLException, IOException {
+
+        JSONObject item = new JSONObject().put("nombre", "update")
+                                          .put("codigo", "1234")
+                                          .put("fecha_v", "2021-03-02")
+                                          .put("categoria", "Nacional")
+                                          .put("precio", 1.200);
+        try{
+        URL url = new URL("http://localhost:8081/productos/2" + item.toString());
+        URI uri = new URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null);
+        HttpUriRequest request = new HttpPut(uri);
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        System.out.println("pasa por el test getproducto");
-        assertThat(httpResponse.getStatusLine().getStatusCode(),equalTo(HttpStatus.SC_NOT_FOUND));
-    }*/
+        }catch (IOException e) {
+            e.printStackTrace();
+        }catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
     
 
 	@Test
@@ -69,7 +72,6 @@ public class junitTest {
         HttpUriRequest request = new HttpDelete( "http://localhost:8081/productos/" + 5 );
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
         HttpEntity httpEntity = httpResponse.getEntity();
-        System.out.println("pasa por el test eliminarproducto");
         System.out.println(EntityUtils.toString(httpEntity));
         }catch (IOException e) {
             e.printStackTrace();
