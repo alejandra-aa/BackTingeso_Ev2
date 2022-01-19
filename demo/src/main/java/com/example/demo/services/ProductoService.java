631448cad2f3655be5e7e1616e98aad6acc76360
sofia <sofia.castro@usach.cl>
2022-01-19 18:36:36 -0300

    Prueba comentario

--- demo/src/main/java/com/example/demo/services/ProductoService.java
+++ demo/src/main/java/com/example/demo/services/ProductoService.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-
+//Miercoles enero 2022.
 @CrossOrigin(origins = "*")
 @RestController
 public class ProductoService {
